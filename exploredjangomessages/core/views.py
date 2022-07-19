17a4aa5d521f2117694ab544d4fb6307c68a6b42
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-07-18 19:17:04 -0500

    add succes messagess

--- exploredjangomessages/core/views.py
+++ exploredjangomessages/core/views.py
@@ -1,3 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    if request.method== 'POST':
+        messages.success(request, 'Profile details updated.', extra_tags='notification')
+
+    return render(request, 'core/index.html')
